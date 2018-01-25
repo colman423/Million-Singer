b3bc0100187fe7eb24b885941cc84038c81d3051
colman423 <colman0423@gmail.com>
2018-01-25 18:23:47 +0800

    Category and Song tab

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ColorManager {
+    public static void setBtnColor(Color normalCol)
+    {
+        Color highlightedCol = getHighlightedColor(normalCol);
+        Color pressedCol = getPressedColor(normalCol);
+
+        GameObject[] btns = GameObject.FindGameObjectsWithTag("Button");
+        foreach( GameObject btn in btns )
+        {
+            ColorBlock colBlock = btn.GetComponent<Button>().colors;
+            colBlock.normalColor = normalCol;
+            colBlock.highlightedColor = highlightedCol;
+            colBlock.pressedColor = pressedCol;
+            btn.GetComponent<Button>().colors = colBlock;
+        }
+    }
+    public static void setBtnDisabledColor(Color disabledCol)
+    {
+        GameObject[] btns = GameObject.FindGameObjectsWithTag("Button");
+        foreach (GameObject btn in btns)
+        {
+            ColorBlock colBlock = btn.GetComponent<Button>().colors;
+            colBlock.disabledColor = disabledCol;
+            btn.GetComponent<Button>().colors = colBlock;
+        }
+    }
+    static Color getHighlightedColor(Color col)
+    {
+        return new Color(col.r * 245 / 255, col.g * 245 / 255, col.b * 245 / 255);
+    }
+    static Color getPressedColor(Color col)
+    {
+        return new Color(col.r * 200 / 255, col.g * 200 / 255, col.b * 200 / 255);
+    }
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        initialConfig();
+	}
+	
+    void initialConfig()
+    {
+        Color btnColor = new Color(0f, 142/(float)255, 224/(float)255);
+        string music = "雨季.wma";
+        ColorManager.setBtnColor(btnColor);
+        ColorManager.setBtnDisabledColor(Color.red);
+    }
+}
