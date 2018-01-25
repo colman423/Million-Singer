1ebcaf21bacfa38d7085838fcee1fc56dbdcf0dc
colman423 <colman0423@gmail.com>
2018-01-26 02:46:37 +0800

    write category name into data

---
+++ Assets/Scripts/Data/WriteHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WriteHandler : MonoBehaviour {
+    public GameObject categoryContainer;
+    public GameObject songContainer;
+
+    public void writeCategory()
+    {
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter("Data/category.txt");
+        foreach ( Transform child in categoryContainer.transform )
+        {
+            string str = child.gameObject.GetComponent<GetSpecificData>().data.GetComponent<Text>().text;
+            writer.WriteLine(str);
+        }
+        writer.Close();
+    }
+}
