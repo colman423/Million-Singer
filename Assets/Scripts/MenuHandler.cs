5b7b13383c582e745640a7080a8b27244d83a457
colman423 <colman0423@gmail.com>
2018-01-27 18:45:09 +0800

    start scene

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
